09fe37a9f06401fd594e868ffeddb29a8c3cbbc0
Marco Hoff <m.hoff@bit-it.com>
2020-10-30 19:29:25 +0100

    First push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require("axios");
+
+// euw1.api.riotgames.com
+// euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/xLosTxSouLi?api_key=RGAPI-66642876-0e6a-413b-b233-e3b558d78f03
+
+const test = async () => {
+  const response = await axios({
+    url:
+      "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/xLosTxSouLi?api_key=RGAPI-66642876-0e6a-413b-b233-e3b558d78f03",
+    method: "get",
+    withCredentials: true,
+    headers: {
+      "Access-Control-Request-Headers": "Content-Type",
+      "Access-Control-Request-Method": "GET"
+    }
+  });
+
+  console.log(response);
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: "",
+      searchResults: []
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  searchForMovies = queryStr => {};
+
+  render() {
+    return (
+      <div className="bg-secondary shadow text-center">
+        <h2 className="bg-danger p-2 rounded shadow-lg">
+          MovieDB-API-Testing von Marco Hoff
+        </h2>
+        <input
+          className="m-2"
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <br />
+        <div className="d-inline">
+          <span>
+            {this.state.inputValue !== "" &&
+              "Nach '" + this.state.inputValue + "' suchen?"}
+          </span>
+          <Button className="m-2" onClick={() => test()}>
+            Test
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
